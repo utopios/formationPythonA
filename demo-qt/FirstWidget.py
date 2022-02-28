8d398ce38b51679ed32c238a06060b81f0bfcd93
ihab <ihab@utopios.net>
2022-02-28 14:06:13 +0100

    QVBoxLayout

--- demo-qt/FirstWidget.py
+++ demo-qt/FirstWidget.py
@@ -1,12 +1,16 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
 
 
 class FirstWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
-        label = QLabel('mon label', self)
+        label = QLabel('mon label')
         label.setAlignment(Qt.AlignHCenter)
-        label.setGeometry(10, 10, 200, 20)
-        button = QPushButton('Yes', self)
-        button.setGeometry(10, 50, 200, 40)
+        #label.setGeometry(10, 10, 200, 20)
+        button = QPushButton('Yes')
+        #button.setGeometry(10, 50, 200, 40)
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(button)
+        self.setLayout(layout)
