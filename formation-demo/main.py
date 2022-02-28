51b71273eac91b502a5ba860ae43f43eba2fffa1
ihab <ihab@utopios.net>
2022-02-28 10:05:37 +0100

    with search

--- formation-demo/main.py
+++ formation-demo/main.py
@@ -4,12 +4,15 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import glob
 import os
+import re
 import shutil
+import sys
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 def use_os_module():
     print(os.getuid())
@@ -18,11 +21,27 @@ def use_os_module():
 def use_shutil_module():
     shutil.copyfile('./data/data.db', 'sauvegarde.db')
 
+
 def use_module_glob():
     print(glob.glob("./cpp/*.cpp"))
 
+
+def use_module_sys():
+    print(sys.argv)
+    sys.stderr.write('Warning possible error\n')
+
+
+def use_module_regex_find_in():
+    print(re.findall(r'\bf[a-z]*', 'wich foot or hand fell fastest'))
+
+
+def use_module_regex_search(regex, chaine):
+    m = re.search(regex, chaine)
+    return m.group(0)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   use_module_glob()
+    print(use_module_regex_search(r'(?<=-)\w+', 'element-test'))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
