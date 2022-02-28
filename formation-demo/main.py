795db5acdf8fce2319f5ae5b710310dae060e3e0
ihab <ihab@utopios.net>
2022-02-28 10:12:39 +0100

    http client

--- formation-demo/main.py
+++ formation-demo/main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import glob
+import http.client
 import os
 import re
 import shutil
@@ -39,9 +40,12 @@ def use_module_regex_search(regex, chaine):
     m = re.search(regex, chaine)
     return m.group(0)
 
+def use_http_client_web(host):
+    connection = http.client.HTTPConnection(host,80)
+    return connection
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(use_module_regex_search(r'(?<=-)\w+', 'element-test'))
+    response = use_http_client_web("http://google.com").getresponse()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
