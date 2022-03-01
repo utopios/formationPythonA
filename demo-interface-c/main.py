2284d8b1e4f6ac5307b4c2215d1e1c89bf3c9b37
ihab <ihab@utopios.net>
2022-03-01 14:43:13 +0100

    utilisation des references dans l'appel de la fonction

--- demo-interface-c/main.py
+++ demo-interface-c/main.py
@@ -5,9 +5,18 @@ def use_c_module():
     c_module = ctypes.CDLL('./../projetc/lib1.so')
     a = ctypes.c_float(10.0)
     b = ctypes.c_float(30.0)
-    c_module.float_add.restype = ctypes.c_float
-    result = c_module.float_add(a, b)
-    print(result)
+    c = ctypes.c_float()
+    # a = 10.0
+    # b = 30.0
+    # c_module.float_add.restype = ctypes.c_float
+    # c_module.float_add.argtypes = [ctypes.c_float, ctypes.c_float]
+    # result = c_module.float_add(a, b)
+    # result2 = c_module.float_add(30.0, 20.0)
+    # print(result)
+    # print(result2)
+    c_module.float_add_ref(ctypes.byref(a), ctypes.byref(b), ctypes.byref(c))
+    print(c.value)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
