827d571860dc059e2c40103da49fcd4778db50f0
ihab <ihab@utopios.net>
2022-03-01 15:53:03 +0100

    exercice appel fonction c dans python

--- demo-todolist/ControllerDb.py
+++ demo-todolist/ControllerDb.py
@@ -4,4 +4,5 @@ class ControllerDb:
 
     def add(self, todo):
         #Logique metier qui communique avec une Api rest
+        #Appeler une fonction dans une librairie en c qui permet d'erire le contenu de la todo dans un fichier.
         return True
